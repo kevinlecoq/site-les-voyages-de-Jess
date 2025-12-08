4453761e1dbc1b0e155544e006bb099498f7bce1
Kevin Lecoq <kevin.lecoq14@gmail.com>
2025-12-08 10:13:01 -0500

    fix: Force redeploy pour variables env

---
+++ insert_admin.sql
@@ -0,0 +1,3 @@
+-- Insérer l'utilisateur admin Jess
+INSERT OR IGNORE INTO admin_users (email, password_hash, name) 
+VALUES ('jessica.finiel@hotmail.com', '$2b$10$J0rmjnrFaDsg2YMpLOCibO9e035Eu.F8kQR2m2Ip573Ah10b3iWXu', 'Jessica');
